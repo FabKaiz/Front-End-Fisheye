83e25c06b23c24e5f53ee6ebb1795611fdc1168f
FabKaiz <cacrfabfr@gmail.com>
2023-04-21 19:41:22 +0200

    [+] - Project innit, fetch data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,64 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const {city, country, id, name, portrait, price, tagline} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/images/ID_pic/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    // Create DOM elements
+    const article = document.createElement('article');
+    const photographerImg = document.createElement('img');
+    const nameH2 = document.createElement('h2');
+    const cityH3 = document.createElement('h3');
+    const taglineP = document.createElement('p');
+    const priceP = document.createElement('p');
+    const link = document.createElement('a');
+
+
+    // Set attributes
+    // Link
+    link.setAttribute("href", `photographer.html?id=${id}`)
+    link.setAttribute("onclick", `window.location.href = 'photographer.html?id=${id}'`)
+    link.setAttribute("class", "photographer_link")
+    link.setAttribute("tabindex", "0")
+    link.setAttribute("aria-label", `Voir la page de ${name}`)
+    link.setAttribute("role", "link")
+    link.setAttribute("alt", `${name}`)
+
+    // Article
+    article.setAttribute("class", "photographer_card")
+    article.setAttribute("id", id)
+
+    // img
+    photographerImg.setAttribute("src", picture)
+    photographerImg.setAttribute("alt", "")
+    photographerImg.setAttribute("class", "photographer_img")
+
+    // Name
+    nameH2.textContent = name;
+    nameH2.setAttribute("class", "photographer_name")
+
+    // City
+    cityH3.textContent = `${city}, ${country}`;
+    cityH3.setAttribute("class", "photographer_city")
+
+    // Tagline
+    taglineP.textContent = tagline;
+    taglineP.setAttribute("class", "photographer_tagline")
+
+    // Price
+    priceP.textContent = `${price}€/jour`;
+    priceP.setAttribute("class", "photographer_price")
+
+    // Append elements
+    link.appendChild(photographerImg);
+    link.appendChild(nameH2);
+    article.appendChild(link);
+    article.appendChild(cityH3);
+    article.appendChild(taglineP);
+    article.appendChild(priceP);
+
+    return (article);
+  }
+
+  return {getUserCardDOM}
 }
