cb0e53320f8ea7767f28d54c53fee64c6033dd2e
FabKaiz <cacrfabfr@gmail.com>
2023-04-28 13:02:47 +0200

    [*] - Formated files

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,32 @@
 async function getPhotographers() {
   // Fetch photographer data
-  const response = await fetch("data/photographers.json")
+  const response = await fetch('data/photographers.json')
 
   // Throw error if response is not ok
   if (!response.ok) {
-    throw new Error("HTTP error " + response.status);
+    throw new Error('HTTP error ' + response.status)
   }
 
   // Return json data
-  return await response.json();
+  return await response.json()
 }
 
 async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+  const photographersSection = document.querySelector('.photographer_section')
 
   photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
+    const photographerModel = photographerFactory(photographer)
+    const userCardDOM = photographerModel.getUserCardDOM()
+    photographersSection.appendChild(userCardDOM)
+  })
 }
 
 async function init() {
   // get photographers data from json file
-  const {photographers} = await getPhotographers();
+  const { photographers } = await getPhotographers()
 
   // display photographers data
-  await displayData(photographers);
+  await displayData(photographers)
 }
-
 
 init()
