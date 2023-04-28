11c59549839890c767de0b8aa1d391a0b5097287
FabKaiz <cacrfabfr@gmail.com>
2023-04-28 13:03:07 +0200

    [*] - Formated files

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,74 +1,73 @@
 function photographerFactory(data) {
-  const {city, country, id, name, portrait, price, tagline} = data;
+  const { city, country, id, name, portrait, price, tagline } = data
 
-  const picture = `assets/images/ID_pic/${portrait}`;
+  const picture = `assets/images/ID_pic/${portrait}`
 
   function getUserCardDOM() {
     // Create DOM elements
-    const article = document.createElement('article');
-    const photographerImg = document.createElement('img');
-    const nameH2 = document.createElement('h2');
-    const cityH3 = document.createElement('h3');
-    const taglineP = document.createElement('p');
-    const priceP = document.createElement('p');
-    const link = document.createElement('a');
-
+    const article = document.createElement('article')
+    const photographerImg = document.createElement('img')
+    const nameH2 = document.createElement('h2')
+    const cityH3 = document.createElement('h3')
+    const taglineP = document.createElement('p')
+    const priceP = document.createElement('p')
+    const link = document.createElement('a')
 
     // Set attributes
     // Link
-    link.setAttribute("href", `photographer.html?id=${id}`)
-    link.setAttribute("class", "photographer_link")
-    link.setAttribute("tabindex", "0")
-    link.setAttribute("aria-label", `Voir la page de ${name}`)
-    link.setAttribute("role", "link")
-    link.setAttribute("alt", `${name}`)
+    link.setAttribute('href', `photographer.html?id=${id}`)
+    link.setAttribute('class', 'photographer_link')
+    link.setAttribute('tabindex', '0')
+    link.setAttribute('aria-label', `Voir la page de ${name}`)
+    link.setAttribute('role', 'link')
+    link.setAttribute('alt', `${name}`)
 
     // link clicked event handler
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
+    link.addEventListener('click', (e) => {
+      e.preventDefault()
       // check if ctrl key is pressed
       if (e.ctrlKey) {
-        window.open(`photographer.html?id=${id}`, '_blank');
-        return;
+        window.open(`photographer.html?id=${id}`, '_blank')
+        return
       }
-      window.location.href = `photographer.html?id=${id}`;
+      window.location.href = `photographer.html?id=${id}`
     })
 
     // Article
-    article.setAttribute("class", "photographer_card")
-    article.setAttribute("id", id)
+    article.setAttribute('class', 'photographer_card')
+    article.setAttribute('id', id)
 
     // img
-    photographerImg.setAttribute("src", picture)
-    photographerImg.setAttribute("alt", "")
-    photographerImg.setAttribute("class", "photographer_img")
+    photographerImg.setAttribute('src', picture)
+    photographerImg.setAttribute('alt', '')
+    photographerImg.setAttribute('class', 'photographer_img')
 
     // Name
-    nameH2.textContent = name;
-    nameH2.setAttribute("class", "photographer_name")
+    nameH2.textContent = name
+    nameH2.setAttribute('class', 'photographer_name')
 
     // City
-    cityH3.textContent = `${city}, ${country}`;
-    cityH3.setAttribute("class", "photographer_city")
+    cityH3.textContent = `${city}, ${country}`
+    cityH3.setAttribute('class', 'photographer_city')
 
     // Tagline
-    taglineP.textContent = tagline;
-    taglineP.setAttribute("class", "photographer_tagline")
+    taglineP.textContent = tagline
+    taglineP.setAttribute('class', 'photographer_tagline')
 
     // Price
-    priceP.textContent = `${price}€/jour`;
-    priceP.setAttribute("class", "photographer_price")
+    priceP.textContent = `${price}€/jour`
+    priceP.setAttribute('class', 'photographer_price')
 
     // Append elements
-    link.appendChild(photographerImg);
-    link.appendChild(nameH2);
-    article.appendChild(link);
-    article.appendChild(cityH3);
-    article.appendChild(taglineP);
-    article.appendChild(priceP);
+    link.appendChild(photographerImg)
+    link.appendChild(nameH2)
+    article.appendChild(link)
+    article.appendChild(cityH3)
+    article.appendChild(taglineP)
+    article.appendChild(priceP)
 
-    return (article);
+    return article
   }
 
-  return {getUserCardDOM}
+  return { getUserCardDOM }
 }
